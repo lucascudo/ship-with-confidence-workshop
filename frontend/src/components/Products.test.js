58b1da4fad3089e1ef596e45f221fa178a29e609
Teixeira Rocha, Lucas <lucas.teixeira.rocha@oracle.com>
2025-05-27 14:00:13 +0100

    Hello Coverage!

--- frontend/src/components/Products.test.js
+++ frontend/src/components/Products.test.js
@@ -3,13 +3,17 @@ import Products from './Products';
 
 describe('Fruits', () => {
     global.fetch = jest.fn();
+    global.console.error = jest.fn();
 
     it('renders fruits data', async () => {
-        // Mocking fetch for unit testing
-        fetch.mockResolvedValueOnce({ json: () => [
+        const products = [
+            { "category": "Fruits", "price": "€1", "stocked": true, "name": "Dragonfruit" },
             { "category": "Fruits", "price": "€4", "stocked": false, "name": "Passionfruit" },
-            { "category": "Vegetables", "price": "€2", "stocked": true, "name": "Spinach" }
-        ]});
+            { "category": "Vegetables", "price": "€2", "stocked": true, "name": "Spinach" },
+            { "category": "Vegetables", "price": "€3", "stocked": false, "name": "Pumpkin" }
+        ];
+        // Mocking fetch for unit testing
+        fetch.mockResolvedValueOnce({ json: () => products });
 
         // Render the component
         render(<Products/>);
@@ -17,18 +21,28 @@ describe('Fruits', () => {
         // Check if the loading state is displayed initially
         expect(screen.getByText('Loading fruits...')).toBeInTheDocument();
 
-        // Wait for the component to render with fetched data
-        const passionfruitElement = await screen.findByText('Passionfruit');
-        const spinachElement = await screen.findByText('Spinach');
+        for (const product of products) {
+            // Wait for the component to render with fetched data
+            const element = await screen.findByText(product.name);
 
-        // Check if the products are displayed correctly
-        expect(spinachElement).toBeInTheDocument();
-        expect(spinachElement.style.color).toBe('');
-        //expect(spinachElement.style.color).not.toBe('red');
-        expect(spinachElement.nextSibling.innerHTML).toBe('€2');
-        expect(passionfruitElement).toBeInTheDocument();
-        expect(passionfruitElement.style.color).toBe('red');
-        //expect(passionfruitElement.nextSibling.innerHTML).toBe('€4'); // will fail
-        expect(passionfruitElement.closest('td').nextSibling.innerHTML).toBe('€4');
+            // Check if the product is displayed correctly
+            expect(element).toBeInTheDocument();
+            expect(element.style.color).toBe(product.stocked ? '' : 'red');
+            const nextSibling = element.stocked ? element.nextSibling : element.closest('td').nextElementSibling;
+            expect(nextSibling.innerHTML).toBe(product.price);
+        }
+    });
+
+    it('catch errors', async () => {
+        // Mocking fetch error
+        fetch.mockImplementationOnce(() => {
+            throw new Error();
+        });
+
+        // Render the component
+        render(<Products/>);
+
+        // Check if the loading state is displayed initially
+        expect(screen.getByText('Loading fruits...')).toBeInTheDocument();
     });
 });
